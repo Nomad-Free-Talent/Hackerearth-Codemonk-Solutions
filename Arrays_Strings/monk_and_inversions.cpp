c659d3de991b8eca6f20245dd911e0bbfce32dcd
Nomad-Free-Talent <nomad.free.talent@gmail.com>
2022-07-22 02:09:07 +0900

    solve: array_string - inversion

---
+++ Arrays_Strings/monk_and_inversions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int T;
+	cin >> T;
+	while (T --) {
+		int N;
+		cin >> N;
+		int** M = new int*[N];
+		int count = 0;
+		for (int i = 0; i < N; i ++) {
+			M[i] = new int[N];
+			for (int j = 0; j < N; j ++) {
+				cin >> M[i][j];
+				for (int k = 0; k <= i; k ++) {
+					for (int l = 0; l <= j; l ++) {
+						if (M[k][l] > M[i][j]) {
+							count ++;
+						}
+					}
+				}
+			}
+		}
+		cout << count << endl;
+	}
+}
